32593d408241f70aaf72bfc31407b69fa97e2c80
chris <chrishaj1191@gmail.com>
2021-10-19 20:29:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Setup app =  new Setup();
+        app.frame.setVisible(true);
+        app.frame.setSize(450,700);
+        app.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+
+
+
+}
